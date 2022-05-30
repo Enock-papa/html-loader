f28f7567f4b2b0a8e25113aa8d5879216ecc8556
Enock-papa <osiirenock11@gmail.com>
2022-05-30 09:24:04 +0300

    loader

---
+++ loader.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>osir #the coder</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<div class="center">
+		<div class="ring"></div>
+		<span>Hello &#128514; World!!</span>
+		
+		
+	</div>
+
+</body>
+</html>
